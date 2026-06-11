80c867f25459dfc43483f757daf00800728ddb53
Rafał <58555259+Bizon15100@users.noreply.github.com>
2026-06-11 14:50:22 +0200

    Update deploy.sh

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -21,6 +21,10 @@ echo "Deploying application..."
 scp -i ~/.ssh/deploy_key .env.prod $EC2_USER@$EC2_HOST:~/.env
 scp -i ~/.ssh/deploy_key docker-compose.yml $EC2_USER@$EC2_HOST:~/
 # Running the application on the server
-ssh -i ~/.ssh/deploy_key $EC2_USER@$EC2_HOST "cd ~/ && docker compose up -d"
+ssh -i ~/.ssh/deploy_key $EC2_USER@$EC2_HOST "
+  docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD} && \
+  docker pull $IMAGE_NAME:$TAG && \
+  cd ~/ && docker compose up -d
+"
 
 echo "Deployment completed successfully"
